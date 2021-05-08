0ebff12960ea12404676a2e6f8d2c7e730a4baf3
Jules Stacy <jules.stacy07@gmail.com>
2021-05-08 13:30:17 -0500

    two agents

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
+import rlgym
+import time
 
 import numpy as np
 import torch
-import rlgym
 import argparse
 import os
 
-import utils
+import agent_utils
 import TD3
 import OurDDPG
 import DDPG
 
 
-# Runs policy for X episodes and returns average reward
-# A fixed seed is used for the eval environment
-def eval_policy(policy, env_name, spawn_opponents, team_size, ep_len_minutes, eval_episodes=10):
-    eval_env = rlgym.make(env_name, spawn_opponents, team_size, ep_len_minutes)
-    #eval_env.seed(seed + 100)
-
-    avg_reward = 0.
-    for _ in range(eval_episodes):
-        state, done = eval_env.reset(), False
-        while not done:
-            action = policy.select_action(np.array(state))
-            state, reward, done, _ = eval_env.step(action)
-            avg_reward += reward
-
-    avg_reward /= eval_episodes
-
-    print("---------------------------------------")
-    print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
-    print("---------------------------------------")
-    return avg_reward
-
-
+#setup name == main
 if __name__ == "__main__":
     #add arguments to parser for future reference throughout main.py
     parser = argparse.ArgumentParser()
     parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
-    parser.add_argument("--env", default="Duel")                    # RLgym environment name (Custom, Duel, Doubles, Standard, Basic)
+    parser.add_argument("--env", default="DuelSelf")                # RLgym environment name (Custom, Duel, Doubles, Standard, Basic)
     parser.add_argument("--opponents", default=True)                # Whether opponents are spawned or not
-    parser.add_argument("--team_size", default=1) # Set team sizes
-    parser.add_argument("--ep_len", default=None)                      # Set a default episode length in minutes
+    parser.add_argument("--team_size", default=1)                   # Set team sizes
+    parser.add_argument("--ep_len", default=None)                   # Set a default episode length in minutes
     parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
     parser.add_argument("--eval_freq", default=5e4, type=int)       # How often (time steps) we evaluate and save model
@@ -68,20 +48,24 @@ if __name__ == "__main__":
     if args.save_model and not os.path.exists("./models"):
         os.makedirs("./models")
 
+    #create the environment
     env = rlgym.make(env_name=args.env, spawn_opponents=args.opponents, team_size=args.team_size, ep_len_minutes=args.ep_len) #make the environment
 
-    # Set seeds
-    #env.seed(args.seed)
-    #env.action_space.seed(seed)
+    #set seeds
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
-    state_dim = env.observation_space.shape[0]
+    #get dimensions of your obervation and action spaces
+    #for further use in setting up your agents
+    #Since DuelSelf will be active, action_space is going to be two controller states
+    #thus we have to reference action_space[0] instead of just action_space
+    obs_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0] 
     max_action = float(env.action_space.high[0])
 
+    #set up your kwargs to feed into your agents
     kwargs = {
-        "state_dim": state_dim,
+        "state_dim": obs_dim,
         "action_dim": action_dim,
         "max_action": max_action,
         "discount": args.discount,
@@ -89,72 +73,84 @@ if __name__ == "__main__":
     }
 
     # Initialize policy
+    #we're starting with TD3
+    #we'll add additional policies later
     if args.policy == "TD3":
         # Target policy smoothing is scaled wrt the action scale
         kwargs["policy_noise"] = args.policy_noise * max_action
         kwargs["noise_clip"] = args.noise_clip * max_action
         kwargs["policy_freq"] = args.policy_freq
         policy = TD3.TD3(**kwargs)
-    elif args.policy == "OurDDPG":
-        policy = OurDDPG.DDPG(**kwargs)
-    elif args.policy == "DDPG":
-        policy = DDPG.DDPG(**kwargs)
+
 
     if args.load_model != "":
         policy_file = file_name if args.load_model == "default" else args.load_model
         policy.load(f"./models/{policy_file}")
 
-    replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
+    # initialize replay buffers for both agents
+    replay_buffer = agent_utils.ReplayBuffer(obs_dim, action_dim)
+    replay_buffer2 = agent_utils.ReplayBuffer(obs_dim, action_dim)
 
-    # Evaluate untrained policy
-    # evaluations = [eval_policy(policy, env_name=args.env, spawn_opponents=args.opponents, team_size=args.team_size, ep_len_minutes=args.ep_len)]
-
-    state, done = env.reset(), False
-    episode_reward = 0
-    episode_timesteps = 0
-    episode_num = 0
-
-    for t in range(int(args.max_timesteps)):
+    
+    episode_num = 0 #initialize the episode number
+    t = 0
+    # begin the training loop
+    while True:
+        # initialize observations
+        obs, done = env.reset(), False
+        obs1 = obs[0]
+        obs2 = obs[1]
+        # initialize rewards
+        episode_reward1 = 0
+        episode_reward2 = 0
+        #initialize more stuff
+        episode_timesteps = 0
+        episode_num += 1 
         
-        episode_timesteps += 1
+        while not done:
+            t += 1
+            #add to the timesteps
+            episode_timesteps += 1
+            
+            # Select action randomly or according to policy
+            if episode_timesteps < args.start_timesteps:
+                action1 = env.action_space.sample()
+                action2 = env.action_space.sample()
+            else:  # Select the action according to policy, with gaussian noise added
+                action1 = (
+                    policy.select_action(np.array(obs1))
+                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
+                ).clip(-max_action, max_action) 
 
-        # Select action randomly or according to policy
-        if t < args.start_timesteps:
-            action = env.action_space.sample()
-        else:
-            action = (
-                policy.select_action(np.array(state))
-                + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-            ).clip(-max_action, max_action)
+                action2 = (
+                    policy.select_action(np.array(obs2))
+                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
+                ).clip(-max_action, max_action)
 
-        # Perform action
-        next_state, reward, done, _ = env.step(action) 
-        # done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
-        done_bool = False
+            # store actions into a controller state list
+            actions = [action1, action2]
 
-        # Store data in replay buffer
-        replay_buffer.add(state, action, next_state, reward, done_bool)
+            # Perform action
+            new_obs, reward, done, state = env.step(actions)
 
-        state = next_state
-        episode_reward += reward
+            # Store data in replay buffers
+            replay_buffer.add(obs1, action1, new_obs[0], reward[0], done)
+            replay_buffer2.add(obs2, action2, new_obs[1], reward[1], done)
 
-        # Train agent after collecting sufficient data
-        if t >= args.start_timesteps:
-            policy.train(replay_buffer, args.batch_size)
+            # update observations
+            obs1 = new_obs[0]
+            obs2 = new_obs[1]
 
-        if done: 
-            # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-            print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
-            # Reset environment
-            state, done = env.reset(), False
-            episode_reward = 0
-            episode_timesteps = 0
-            episode_num += 1 
+            episode_reward1 += reward[0]
+            episode_reward2 += reward[1]
 
-        # Evaluate episode and save model
-        if (t + 1) % args.eval_freq == 0:
-           # evaluations.append(eval_policy(policy, args.env, args.seed))
-           # np.save(f"./results/{file_name}", evaluations)
-            if args.save_model: policy.save(f"./models/{file_name}")
+            # Train agent after collecting sufficient data
+            if episode_timesteps >= args.start_timesteps:
+                policy.train(replay_buffer, args.batch_size)
 
-        
+        print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Blue Rwrd: {episode_reward1:.3f} Red Rwrd: {episode_reward2:.3f}")
+
+
+
+
+
