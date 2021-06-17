1eb0157937ef9a01647e117f45b2a15b1983de5d
daaneshi <57160399+daaneshi@users.noreply.github.com>
2021-06-17 11:31:35 -0500

    Add files via upload
    added main.py set up to run PPO with 3 instances of RLGym (6 agents). Check filepath locations for saving logs/policies.

--- main.py
+++ main.py
@@ -1,68 +1,21 @@
-from rlgym.utils import reward_functions
-from rlgym.utils.reward_functions import combined_reward
-from rlgym.utils.terminal_conditions.common_conditions import GoalScoredCondition, TimeoutCondition
-from rlgym.utils.reward_functions.common_rewards import TouchBallReward
-from rlgym.utils.reward_functions.shoot_ball_reward import ShootBallReward
+from rlgym.utils.terminal_conditions.common_conditions import GoalScoredCondition
+from stable_baselines3.common.callbacks import CheckpointCallback
 from rlgym.utils.reward_functions.combined_reward import CombinedReward
-from cust_reward import GoalReward
+from stable_baselines3 import PPO
+from rlgym.utils.reward_functions.common_rewards import ConstantReward
+from rlgym.utils.obs_builders.advanced_obs import AdvancedObs
+from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition
+from rlgym.wrappers.sb3_wrappers import SB3MultipleInstanceWrapper, SB3VecMonitor
+from rlgym.utils.reward_functions.common_rewards import EventReward
+from rlgym.utils.reward_functions.common_rewards import RewardIfBehindBall
+from rlgym.utils.reward_functions.common_rewards import VelocityPlayerToBallReward
+from rlgym.utils.reward_functions.common_rewards import SaveBoostReward
 
-
-import rlgym
-# import time
-
-import numpy as np
-import torch
-import argparse
-import os
-
-import agent_utils
-import TD3
-# import OurDDPG
-# import DDPG
-
-
-#setup name == main
-if __name__ == "__main__":
-    #add arguments to parser for future reference throughout main.py
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
-    parser.add_argument("--env", default="DuelSelf")                # RLgym environment name (Custom, Duel, Doubles, Standard, Basic)
-    parser.add_argument("--opponents", default=True)                # Whether opponents are spawned or not
-    parser.add_argument("--team_size", default=1)                   # Set team sizes
-    parser.add_argument("--ep_len", default=None)                   # Set a default episode length in minutes
-    parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
-    parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
-    parser.add_argument("--eval_freq", default=5e4, type=int)       # How often (time steps) we evaluate and save model
-    parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
-    parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
-    parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
-    parser.add_argument("--discount", default=0.99)                 # Discount factor
-    parser.add_argument("--tau", default=0.005)                     # Target network update rate
-    parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
-    parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
-    parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
-    parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
-    parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
-    args = parser.parse_args() #add all the arguments to an args object
-
-    file_name = f"{args.policy}_{args.env}_{args.seed}"
-    print("---------------------------------------")
-    print(f"Policy: {args.policy}, Env: {args.env}, Seed: {args.seed}")
-    print("---------------------------------------")
-
-    #create results storage and model storage
-    if not os.path.exists("./results"):
-        os.makedirs("./results")
-
-    if args.save_model and not os.path.exists("./models"):
-        os.makedirs("./models")
-
-
-    # set episode timeout
-    # 1800 sec = 30 min
+if __name__ == '__main__':
+    # set episode timeout (1800 sec = 30 min)
     default_tick_skip = 8
     physics_ticks_per_second = 120
-    ep_len_seconds = 1800
+    ep_len_seconds = 150
     max_steps = int(round(ep_len_seconds * physics_ticks_per_second / default_tick_skip))
 
     # specify terminal conditions from commons
@@ -70,117 +23,103 @@ if __name__ == "__main__":
     term_cond2 = GoalScoredCondition()
 
     # specify reward functions
-    
-    goal_reward = ShootBallReward()
-    touch_reward = TouchBallReward()
-
-    all_rewards = CombinedReward(reward_functions=(goal_reward, touch_reward), reward_weights=(1.0,1.0))
-    
-    
-    
-    #create the environment
-    env = rlgym.make(env_name=args.env, spawn_opponents=args.opponents, team_size=args.team_size, ep_len_minutes=args.ep_len, terminal_conditions=[term_cond2], reward_fn=all_rewards) #make the environment | 
-
-    #set seeds
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
-
-    #get dimensions of your obervation and action spaces
-    #for further use in setting up your agents
-    #Since DuelSelf will be active, action_space is going to be two controller states
-    #thus we have to reference action_space[0] instead of just action_space
-    obs_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0] 
-    max_action = float(env.action_space.high[0])
-
-    #set up your kwargs to feed into your agents
-    kwargs = {
-        "state_dim": obs_dim,
-        "action_dim": action_dim,
-        "max_action": max_action,
-        "discount": args.discount,
-        "tau": args.tau,
-    }
-
-    # Initialize policy
-    #we're starting with TD3
-    #we'll add additional policies later
-    if args.policy == "TD3":
-        # Target policy smoothing is scaled wrt the action scale
-        kwargs["policy_noise"] = args.policy_noise * max_action
-        kwargs["noise_clip"] = args.noise_clip * max_action
-        kwargs["policy_freq"] = args.policy_freq
-        policy = TD3.TD3(**kwargs)
+    fps = 15
+    obs_builder = AdvancedObs()
+    all_rewards = CombinedReward((EventReward(goal=85, team_goal=0, concede=-55, touch=1, shot=10, save=25, demo=10),
+                                  VelocityPlayerToBallReward(use_scalar_projection=False),
+                                  RewardIfBehindBall(ConstantReward()),
+                                  SaveBoostReward()),
+                                 (1,
+                                  (0.1 / fps),
+                                  (0.01 / fps),
+                                  (0.05 / fps)))
 
 
-    if args.load_model != "":
-        policy_file = file_name if args.load_model == "default" else args.load_model
-        policy.load(f"./models/{policy_file}")
+    # BallYCoordinateReward(),
+    # RewardIfBehindBall(ConstantReward()),
+    # VelocityPlayerToBallReward(use_scalar_projection=False),
+    # SaveBoostReward(),
+    # VelocityReward(),
+    # FaceBallReward()),
 
-    # initialize replay buffers for both agents
-    replay_buffer = agent_utils.ReplayBuffer(obs_dim, action_dim)
-    replay_buffer2 = agent_utils.ReplayBuffer(obs_dim, action_dim)
+    # reward_weights=(1))
 
-    
-    episode_num = 0 #initialize the episode number
-    t = 0
-    # begin the training loop
-    while True:
-        # initialize observations
-        obs, done = env.reset(), False
-        obs1 = obs[0]
-        obs2 = obs[1]
-        # initialize rewards
-        episode_reward1 = 0
-        episode_reward2 = 0
-        #initialize more stuff
-        episode_timesteps = 0
-        episode_num += 1 
-        
-        while not done:
-            t += 1
-            #add to the timesteps
-            episode_timesteps += 1
-            
-            # Select action randomly or according to policy
-            if t < args.start_timesteps:
-                action1 = env.action_space.sample()
-                action2 = env.action_space.sample()
-            else:  # Select the action according to policy, with gaussian noise added
-                action1 = (
-                    policy.select_action(np.array(obs1))
-                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-                ).clip(-max_action, max_action) 
+    # 0.1 / fps,
+    # 0.01 / fps,
+    # 0.1 / fps / 23,
+    # 0.01 / fps,
+    # 1,
+    # 1))
 
-                action2 = (
-                    policy.select_action(np.array(obs2))
-                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-                ).clip(-max_action, max_action)
+    # def get_match_args():
+    #    return dict(
+    #        team_size=1,
+    #        tick_skip=8,
+    #        reward_function=all_rewards,
+    #        self_play=True,
+    #        terminal_conditions=[TimeoutCondition(900), GoalScoredCondition()],
+    #        obs_builder=obs_builder)
+    # learner = PPO.load('C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\ppo_bot__1000002_steps')
+    # okgo = learner.policy
 
-            # store actions into a controller state list
-            actions = [action1, action2]
-
-            # Perform action
-            new_obs, reward, done, state = env.step(actions)
-
-            # Store data in replay buffers
-            replay_buffer.add(obs1, action1, new_obs[0], reward[0], done)
-            replay_buffer2.add(obs2, action2, new_obs[1], reward[1], done)
-
-            # update observations
-            obs1 = new_obs[0]
-            obs2 = new_obs[1]
-
-            episode_reward1 += reward[0]
-            episode_reward2 += reward[1]
-
-            # Train agent after collecting sufficient data
-            if t >= args.start_timesteps:
-                policy.train(replay_buffer, args.batch_size)
-
-        print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Blue Rwrd: {episode_reward1:.3f} Red Rwrd: {episode_reward2:.3f}")
+    def get_match_args():
+        return dict(
+            team_size=1,
+            tick_skip=8,
+            reward_function=all_rewards,
+            self_play=True,
+            terminal_conditions=[term_cond1],
+            obs_builder=obs_builder)
 
 
+    epic_path = r"Z:\Epic Games\rocketleague\Binaries\Win64\RocketLeague.exe"
+    num_procs = 3
+    num_ts = 25_000_000
+    env = SB3VecMonitor(SB3MultipleInstanceWrapper(epic_path, num_procs, get_match_args, wait_time=45),
+                        filename='C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\Reward_Details')
 
+    # env = Monitor(rlgym.make("Duel", spawn_opponents=True, team_size=1, ep_len_minutes=1,
+    #                 obs_builder=obs_builder, terminal_conditions=[term_cond1], reward_fn=all_rewards, game_speed=1))
 
+    checkpoint = CheckpointCallback(save_freq=1_000_000 // env.num_envs + 1,
+                                    save_path='C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\',
+                                    name_prefix="ppo_bot_",
+                                    verbose=1)
 
+    #learner = PPO("MlpPolicy", env, verbose=3, device='cuda', n_epochs=1, target_kl=0.02 / 1.5)
+    learner = PPO.load(path='C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\ppo_bot__70_000_140_steps', env=env, device='cuda', n_epochs=1, target_kl=0.02 / 1.5)
+    learner.learn(total_timesteps=num_ts, callback=checkpoint)
+    # learner = PPO(env=env, verbose=1, device='cuda', policy_kwargs=DaanPolicy)
+    # learner.learn(total_timesteps=num_ts, callback=checkpoint)
+    # learner.save('C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\ppo_bot_175_000_000')
+    # DaanPolicy = dict(policy=okgo)
+    # model = PPO("MlpPolicy", env,
+    # verbose=3, device="cpu")
+    # model.learn(100_000_000)
+    # model.save("policy")
+    # create the environment
+    # log_dir = "tmp/"
+    # os.makedirs(log_dir, exist_ok=True)
+    # env = SubprocVecEnvWrapper(rlgym.make("DuelSelf", spawn_opponents=True, team_size=1,
+    #                               ep_len_minutes=1, obs_builder=obs_builder, terminal_conditions=[term_cond1],
+    #                              reward_fn=all_rewards), num_instances=3)  # make the environment |
+
+    # env = VecEnvWrapper(rlgym.make("DuelSelf", spawn_opponents=True, team_size=1, ep_len_minutes=1,
+    # obs_builder=obs_builder, terminal_conditions=[term_cond1,term_cond2], reward_fn=all_rewards))  # make the
+    # environment | env = Monitor(env, log_dir)
+
+    # model = PPO("MlpPolicy", env, n_epochs=1, target_kl=0.02 / 1.5, learning_rate=1e-4, ent_coef=0.01, vf_coef=1,
+    # gamma=0.995, verbose=3, batch_size=128, n_steps=2048, tensorboard_log="./logs/", device="cuda") checkpoint =
+    # CheckpointCallback(10_000_000 // env.num_envs + 1, "policy")  # Only increments once all agents take step
+    # model.learn(100_000_000, callback=checkpoint) model = PPO.load(
+    # 'C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\ppo_bot_first', env=env, verbose=1,
+    # tensorboard_log="./ppo_rl_tensorboard/", device='cuda') model.learn(total_timesteps=39e5, callback=)
+    # model.save('C:\\Users\\Daanesh\\PycharmProjects\\RocketLeagueRL\\PPO\\ppo_bot_first')
+
+    # policy = learner.policy
+
+    # obs = env.reset()
+    # for i in range(5000):
+    #    action, _states = learner.predict(obs, deterministic=True)
+    #    obs, rewards, dones, info = env.step(action)
+    # env.render()
